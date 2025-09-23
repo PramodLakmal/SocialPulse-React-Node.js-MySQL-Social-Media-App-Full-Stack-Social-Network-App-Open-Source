679abaff7bc65dd87604700f33365e49fba21ec3
Nipun Lahiru <nipunlahiru6902@gmail.com>
2025-09-23 23:50:26 +0530

    Missing Anti-clickjacking Header  valnerability fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'X-Frame-Options': 'DENY',
+      "Content-Security-Policy": "frame-ancestors 'none'",
+    },
+  },
+  preview: {
+    headers: {
+      'X-Frame-Options': 'DENY',
+      "Content-Security-Policy": "frame-ancestors 'none'",
+    },
+  },
 })
